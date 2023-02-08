c9e0f4b44b6a4aea686147cc0642c019bff11c8e
Steven Haye <8312553+sphred5@users.noreply.github.com>
2023-02-07 22:39:30 -0800

    added styles for products page and nav

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,5 +1,7 @@
+import styled from 'styled-components';
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import Product from './Product';
 
 
 const ALL_PRODUCTS_QUERY = gql`
@@ -18,7 +20,11 @@ query ALL_PRODUCTS_QUERY {
   }
 }
 `
-
+const ProductsListStyles = styled.div`
+  display:grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;  
+`;
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   // console.log(data, error, loading);
@@ -26,11 +32,11 @@ export default function Products() {
   if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
-      <div>
+      <ProductsListStyles>
         {data.products.map(product => (
-         <p key={product.id}>{product.name}</p> 
+          <Product key={product.id} product={product}/>
         ))}
-      </div>
+      </ProductsListStyles>
     </div>
   );
 }
